25b5d9d9f2b4873ac6963f8a5b151c6a8ca85284
Krypt0n123 <352600525@qq.com>
2026-01-25 00:17:31 +0800

    Connection timeout

--- Snippetbox/cmd/web/main.go
+++ Snippetbox/cmd/web/main.go
@@ -52,7 +52,7 @@ func main() {
 	sessionManager := scs.New()
 	sessionManager.Store = mysqlstore.New(db)
 	sessionManager.Lifetime = 12 * time.Hour
-	sessionManager.Cookie.Secure=true
+	sessionManager.Cookie.Secure = true
 
 	app := &application{
 		errorLog:       errorLog,
@@ -63,19 +63,22 @@ func main() {
 		sessionManager: sessionManager,
 	}
 
-	tlsConfig:=&tls.Config{
-		CurvePreferences: []tls.CurveID{tls.X25519,tls.CurveP256},
+	tlsConfig := &tls.Config{
+		CurvePreferences: []tls.CurveID{tls.X25519, tls.CurveP256},
 	}
 
 	srv := &http.Server{
-		Addr:     *addr,
-		ErrorLog: errorLog,
-		Handler:  app.routes(),
-		TLSConfig: tlsConfig,
+		Addr:         *addr,
+		ErrorLog:     errorLog,
+		Handler:      app.routes(),
+		TLSConfig:    tlsConfig,
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
 	}
 
 	infoLog.Printf("server start on https://localhost%s", *addr)
-	err = srv.ListenAndServeTLS("./tls/cert.pem","./tls/key.pem")
+	err = srv.ListenAndServeTLS("./tls/cert.pem", "./tls/key.pem")
 	errorLog.Fatal(err)
 }
 
