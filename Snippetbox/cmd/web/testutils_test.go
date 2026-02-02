73794e6b487250ec92170b844b9bace2378a2a7a
Krypt0n123 <352600525@qq.com>
2026-02-03 06:47:24 +0800

    Mocking dependencies

--- Snippetbox/cmd/web/testutils_test.go
+++ Snippetbox/cmd/web/testutils_test.go
@@ -8,12 +8,34 @@ import (
 	"net/http/cookiejar"
 	"net/http/httptest"
 	"testing"
+	"time"
+
+	"snippetbox/internal/models/mocks"
+
+	"github.com/alexedwards/scs/v2"
+	"github.com/go-playground/form/v4"
 )
 
 func newTestApplication(t *testing.T) *application {
+	templateCache, err := newTemplateCache()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	formDecoder := form.NewDecoder()
+
+	sessionManager := scs.New()
+	sessionManager.Lifetime = 12 * time.Hour
+	sessionManager.Cookie.Secure = true
+
 	return &application{
-		errorLog: log.New(io.Discard, "", 0),
-		infoLog:  log.New(io.Discard, "", 0),
+		errorLog:       log.New(io.Discard, "", 0),
+		infoLog:        log.New(io.Discard, "", 0),
+		snippets:       &mocks.SnippetModel{},
+		users:         	&mocks.UserModel{},
+		templateCache:  templateCache,
+		formDecoder:    formDecoder,
+		sessionManager: sessionManager,
 	}
 }
 
@@ -24,17 +46,17 @@ type testServer struct {
 func newTestServer(t *testing.T, h http.Handler) *testServer {
 	ts := httptest.NewTLSServer(h)
 
-	jar,err:=cookiejar.New(nil)
+	jar, err := cookiejar.New(nil)
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	ts.Client().Jar=jar
+	ts.Client().Jar = jar
 
-	ts.Client().CheckRedirect=func(req *http.Request, via []*http.Request) error{
+	ts.Client().CheckRedirect = func(req *http.Request, via []*http.Request) error {
 		return http.ErrUseLastResponse
 	}
-	
+
 	return &testServer{ts}
 }
 
