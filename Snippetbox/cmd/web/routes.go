2f144ff17ac63942451aa658f7ee91fff0aa20fa
Krypt0n123 <352600525@qq.com>
2026-02-03 01:33:55 +0800

    end to end testing

--- Snippetbox/cmd/web/routes.go
+++ Snippetbox/cmd/web/routes.go
@@ -17,8 +17,9 @@ func (app *application) routes() http.Handler {
 	})
 
 	fileServer := http.FileServer(http.FS(ui.Files))
-
 	router.Handler(http.MethodGet, "/static/*filepath", fileServer)
+
+	router.HandlerFunc(http.MethodGet, "/ping", ping)
 
 	dynamic := alice.New(app.sessionManager.LoadAndSave, noSurf, app.authenticate)
 
